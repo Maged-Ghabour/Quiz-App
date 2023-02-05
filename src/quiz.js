2b2bf41193618f31bff8faf537445b85e8e6ccad
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2023-02-05 20:50:59 +0200

    Create Quiz App

---
+++ src/quiz.js
@@ -0,0 +1,190 @@
+export let allQuestions =
+    [{
+        question: "Which of the below is responsible for controlling the size of sent packets of data?",
+        options: [
+            "NAT",
+            "MTU",
+            "DNS",
+            "DHCP"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "ADSL and VDSL CPE WAN configuration, is the same settings?",
+        options: [
+            "True",
+            "False"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "Which from the below is WE DNS :",
+        options: [
+            "8.8.8.8-8.8.4.4",
+            "163.121.128.137-163.121.128.138",
+            "8.8.4.4-8.8.8.8",
+            "163.121.128.134-163.121.128.135"
+
+        ]
+        ,
+        answer: 3,
+        selected: null
+    }, {
+        question: "The Ethernet cable ends with RJ :",
+        options: [
+            "40",
+            "45",
+            "11",
+            "12"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "The device that split data and voice is ......",
+        options: [
+            "Rosetta",
+            "Splitter",
+            "Filtter"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "Which of the following is APIPA IP :",
+        options: [
+            "192.168.8.1",
+            "169.254.0.1",
+            "192.168.1.1",
+            "192.168.1.1"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "Which of the below is responsible for translating the Public IP into a Private one and Vice Versa?",
+        options: [
+            "DNS",
+            "MTU",
+            "NAT",
+            "DHCP"
+        ]
+        ,
+        answer: 2,
+        selected: null
+    },{
+        question: "CST has Voice Down and has landline bills with the amount of 65 LE also passed the grace period, right action is :",
+        options: [
+            "Re-Troubleshoot as “Voice Verification",
+            "Inform CST to connect from his main",
+            "Re-escalate to IU Direct"
+        ]
+        ,
+        answer: 2,
+        selected: null
+    }, {
+        question: "We can check Landline bills from .....",
+        options: [
+            "I Report / Cases",
+            "OSS  / TE website",
+            "BSS"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "In voice verification, landline bills from TE website: no bills, voice status checked: not sure, phone set avail: no, called customer landline : recorded MSG, the line is temporarily out of service, your action :",
+        options: [
+            "Consider as voice up",
+            "Consider as voice Down",
+            "Transfer CST to fixed line Q"
+        ]
+        ,
+        answer: 1,
+        selected: null
+    }, {
+        question: "Customer Follow us complaining that there are stolen cable in his POP while no outage updated in CST 360, so CCA should follow normal troubleshoot and escalate normal if needed",
+        options: [
+            "True",
+            "False",
+
+        ]
+        ,
+        answer: 0,
+        selected: null
+    }, {
+        question: "Customer Follow us complaining that there are stolen cable in his POP while no outage updated in CST 360, so CCA should follow normal troubleshoot and escalate normal if needed",
+        options: [
+            "True",
+            "False",
+
+        ]
+        ,
+        answer: 0,
+        selected: null
+    }, {
+        question: "Voice Overlapping case happened between two numbers at different Exchanges",
+        options: [
+            "True",
+            "False",
+
+        ]
+        ,
+        answer: 1,
+        selected: null
+    },
+    , {
+        question: "Customer subscribed on Caller ID service and not working with him, right action is : ",
+        options: [
+            "Transfer CST to Fixed line Q",
+            "Escalate to IU" ,
+            "Inform CST to connect from main"
+      
+
+        ]
+        ,
+        answer: 0,
+        selected: null
+    },
+    , {
+        question: "In voice verification, landline bills from TE website: no bills, voice status checked: not sure, phone set avail: no, called customer landline : recorded MSG, the line is temporarily out of service, your action :",
+        options: [
+            "Consider as voice up",
+            "Transfer CST to fixed line Q",
+            "Consider as voice Down"
+            
+
+        ]
+        ,
+        answer: 2,
+        selected: null
+    },
+    , {
+        question: "Customer Follow us complaining that there are stolen cable in his POP while no outage updated in CST 360, so CCA should follow normal troubleshoot and escalate normal if needed",
+        options: [
+            "True",
+            "False",
+
+        ]
+        ,
+        answer: 0,
+        selected: null
+    },
+    , {
+        question: "Customer Follow us complaining that there are stolen cable in his POP while no outage updated in CST 360, so CCA should follow normal troubleshoot and escalate normal if needed",
+        options: [
+            "True",
+            "False",
+
+        ]
+        ,
+        answer: 0,
+        selected: null
+    }
+    ]
+
+
+
+
